a9b16aed08bc55d49f9a26db416d82d35a5b27a3
ac-kavali <ahmadachahi400@gmail.com>
2026-04-05 06:22:05 +0100

    Restructure project: reorganized into media/css/html/scripts folders, updated paths

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+// const imgs = ["rock.png", "paper.png", "scissors.png"];
+// let currentIndex = 0;
+// const playBtn = document.getElementById("play-btn")
+
+// const intervalID = setInterval(() => {
+//     currentIndex = (currentIndex + 1) % imgs.length;
+//     document.getElementById("randomImg").src = imgs[currentIndex];
+// }, 2000);
+
